a72c0596ffff576c13b472e9b73472d5f82ae9a7
johnaduke <73294147+johnaduke@users.noreply.github.com>
2021-08-09 15:59:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,13 @@ resource "aws_security_group" "sg_22" {
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
+	
+ingress {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
